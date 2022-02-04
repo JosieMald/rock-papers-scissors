1aea289ccce35f6b545746f05b8da92312054279
Lucre <josielindamaldonado@gmail.com>
2022-02-04 14:08:49 -0500

    added user input and changed to uppercase.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+var computerOptions = ["R", "P", "S"];
+
+var userInput = prompt("Enter R, P, S:");
+
+console.log(userInput);
+
+var useResult = userInput.toUpperCase()
+
+console.log(useResult);
