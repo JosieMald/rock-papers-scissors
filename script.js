98f807f4a16b4c38725f8e95c8727a8aed4fa118
Lucre <josielindamaldonado@gmail.com>
2022-02-07 19:29:36 -0500

    fixed stats alert.

--- script.js
+++ script.js
@@ -2,12 +2,32 @@ var wins = 0;
 var losses = 0;
 var ties = 0;
 
-var computerOptions = ["R", "P", "S"];
+// var computerOptions = ["R", "P", "S"];
 
-var userInput = prompt("Enter R, P, S:");
+// startGame();
 
-console.log(userInput);
+// function startGame() {
+//   var userInput = prompt("Enter R, P, S:");
+//   var useResult = userInput.toUpperCase();
+//   if (computerOptions.includes(useResult)) {
+//     random(useResult);
+//   } else {
+//     alert("That was an invalid entry. Please type R, P, S and try again");
+//     startGame();
+//   }
+// }
 
-var useResult = userInput.toUpperCase()
-
-console.log(useResult);
+// function random(useResult) {
+//   var num = Math.floor(Math.random() * 3);
+//   alert("The computer chose " + computerOptions[num]);
+//   if (computerOptions[num] === useResult) {
+//     alert("It's a tie!");
+//     ties++;
+//     displayStats();
+//   }
+// }
+displayStats()
+function displayStats() {
+    var msg = "Stats: \nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties ;
+  alert(msg);
+}
