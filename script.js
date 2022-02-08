40b4a6fd5a2bb28da7587085c319d2ff51ba3f33
Lucre <josielindamaldonado@gmail.com>
2022-02-07 21:01:25 -0500

    added win or lose comparison, added comments, and formated document.

--- script.js
+++ script.js
@@ -4,45 +4,81 @@ var ties = 0;
 
 var computerOptions = ["R", "P", "S"];
 
-// startGame();
+// START OF THE GAME -----------------------------------------------------------------------
 
-// function startGame() {
-//   var userInput = prompt("Enter R, P, S:");
-//   var useResult = userInput.toUpperCase();
-//   if (computerOptions.includes(useResult)) {
-//     random(useResult);
-//   } else {
-//     alert("That was an invalid entry. Please type R, P, S and try again");
-//     startGame();
-//   }
-// }
-
-// function random(useResult) {
-//   var num = Math.floor(Math.random() * 3);
-//   alert("The computer chose " + computerOptions[num]);
-//   if (computerOptions[num] === useResult) {
-//     alert("It's a tie!");
-//     ties++;
-//     displayStats();
-//   }
-// }
-
-displayStats()
-function displayStats() {
-    var msg = "Stats: \nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties ;
-  alert(msg);
+startGame();
+function startGame() {
+  var userInput = prompt("Enter R, P, S:");
+  var useResult = userInput.toUpperCase();
+  if (computerOptions.includes(useResult)) {
+    random(useResult);
+  } else {
+    alert("That was an invalid entry. Please type R, P, S and try again");
+    startGame();
+  }
 }
-playAgain();
+
+// COMPUTER CHOOSES LETTER -----------------------------------------------------------------------
+
+function random(useResult) {
+  var num = Math.floor(Math.random() * 3);
+  alert("The computer chose " + computerOptions[num]);
+  if (computerOptions[num] !== useResult) {
+    computerAnswer = computerOptions[num];
+    comparison(computerAnswer, useResult);
+  }
+  if (computerOptions[num] === useResult) {
+    alert("It's a tie!");
+    ties++;
+    displayStats();
+  }
+}
+
+// LOSE OR WIN COMPARISON -----------------------------------------------------------------------
+
+function comparison(computerAnswer, useResult) {
+  if (
+    (computerAnswer == "R" && useResult == "P") ||
+    (computerAnswer == "S" && useResult == "R") ||
+    (computerAnswer == "P" && useResult == "S")
+  ) {
+    alert("You won!");
+    wins++;
+    displayStats();
+  } else if (
+    (computerAnswer == "P" && useResult == "R") ||
+    (computerAnswer == "R" && useResult == "S") ||
+    (computerAnswer == "S" && useResult == "P")
+  ) {
+    alert("You lost!");
+    losses++;
+    displayStats();
+  }
+}
+
+// DISPLAYS STATS AFTER USER INPUT -----------------------------------------------------------------------
+
+function displayStats() {
+  var msg =
+    "Stats: \nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties;
+  alert(msg);
+  playAgain();
+}
+
+// ASKS USER TO PLAY AGAIN -----------------------------------------------------------------------
 
 function playAgain() {
-    if (confirm("Would you like to play again?") == true) {
-        startGame();
-    } else {
-        finalStats()
-    }
+  if (confirm("Would you like to play again?") == true) {
+    startGame();
+  } else {
+    finalStats();
+  }
 }
-// 
+
+// DISPLAYS FINAL STATS -----------------------------------------------------------------------
+
 function finalStats() {
-    var msg = "Stats: \nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties ;
+  var msg =
+    "Final Stats: \nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties;
   alert(msg);
 }
